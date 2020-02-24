6da549c62187d482b8fcd3180cb2f1d1ed96c95d
rrooongseang <rroongseang@uchicago.edu>
2020-02-23 23:12:34 -0600

    switched to sql file

---
+++ bigquery_union_statement.sql
@@ -0,0 +1,113 @@
+select * from `fh-bigquery.reddit_comments.2015_01` where subreddit = 'politics'
+union all
+select * from `fh-bigquery.reddit_comments.2015_02` where subreddit = 'politics'
+union all
+select * from `fh-bigquery.reddit_comments.2015_03` where subreddit = 'politics'
+union all
+select * from `fh-bigquery.reddit_comments.2015_04` where subreddit = 'politics'
+union all
+select * from `fh-bigquery.reddit_comments.2015_05` where subreddit = 'politics'
+union all
+select * from `fh-bigquery.reddit_comments.2015_06` where subreddit = 'politics'
+union all
+select * from `fh-bigquery.reddit_comments.2015_07` where subreddit = 'politics'
+union all
+select * from `fh-bigquery.reddit_comments.2015_08` where subreddit = 'politics'
+union all
+select * from `fh-bigquery.reddit_comments.2015_09` where subreddit = 'politics'
+union all
+select * from `fh-bigquery.reddit_comments.2015_10` where subreddit = 'politics'
+union all
+select * from `fh-bigquery.reddit_comments.2015_11` where subreddit = 'politics'
+union all
+select * from `fh-bigquery.reddit_comments.2015_12` where subreddit = 'politics'
+union all
+select * from `fh-bigquery.reddit_comments.2016_01` where subreddit = 'politics'
+union all
+select * from `fh-bigquery.reddit_comments.2016_02` where subreddit = 'politics'
+union all
+select * from `fh-bigquery.reddit_comments.2016_03` where subreddit = 'politics'
+union all
+select * from `fh-bigquery.reddit_comments.2016_04` where subreddit = 'politics'
+union all
+select * from `fh-bigquery.reddit_comments.2016_05` where subreddit = 'politics'
+union all
+select * from `fh-bigquery.reddit_comments.2016_06` where subreddit = 'politics'
+union all
+select * from `fh-bigquery.reddit_comments.2016_07` where subreddit = 'politics'
+union all
+select * from `fh-bigquery.reddit_comments.2016_08` where subreddit = 'politics'
+union all
+select * from `fh-bigquery.reddit_comments.2016_09` where subreddit = 'politics'
+union all
+select * from `fh-bigquery.reddit_comments.2016_10` where subreddit = 'politics'
+union all
+select * from `fh-bigquery.reddit_comments.2016_11` where subreddit = 'politics'
+union all
+select * from `fh-bigquery.reddit_comments.2016_12` where subreddit = 'politics'
+union all
+select * from `fh-bigquery.reddit_comments.2017_01` where subreddit = 'politics'
+union all
+select * from `fh-bigquery.reddit_comments.2017_02` where subreddit = 'politics'
+union all
+select * from `fh-bigquery.reddit_comments.2017_03` where subreddit = 'politics'
+union all
+select * from `fh-bigquery.reddit_comments.2017_04` where subreddit = 'politics'
+union all
+select * from `fh-bigquery.reddit_comments.2017_05` where subreddit = 'politics'
+union all
+select * from `fh-bigquery.reddit_comments.2017_06` where subreddit = 'politics'
+union all
+select * from `fh-bigquery.reddit_comments.2017_07` where subreddit = 'politics'
+union all
+select * from `fh-bigquery.reddit_comments.2017_08` where subreddit = 'politics'
+union all
+select * from `fh-bigquery.reddit_comments.2017_09` where subreddit = 'politics'
+union all
+select * from `fh-bigquery.reddit_comments.2017_10` where subreddit = 'politics'
+union all
+select * from `fh-bigquery.reddit_comments.2017_11` where subreddit = 'politics'
+union all
+select * from `fh-bigquery.reddit_comments.2017_12` where subreddit = 'politics'
+union all
+select * from `fh-bigquery.reddit_comments.2018_01` where subreddit = 'politics'
+union all
+select * from `fh-bigquery.reddit_comments.2018_02` where subreddit = 'politics'
+union all
+select * from `fh-bigquery.reddit_comments.2018_03` where subreddit = 'politics'
+union all
+select * from `fh-bigquery.reddit_comments.2018_04` where subreddit = 'politics'
+union all
+select * from `fh-bigquery.reddit_comments.2018_05` where subreddit = 'politics'
+union all
+select * from `fh-bigquery.reddit_comments.2018_06` where subreddit = 'politics'
+union all
+select * from `fh-bigquery.reddit_comments.2018_07` where subreddit = 'politics'
+union all
+select * from `fh-bigquery.reddit_comments.2018_08` where subreddit = 'politics'
+union all
+select * from `fh-bigquery.reddit_comments.2018_09` where subreddit = 'politics'
+union all
+select * from `fh-bigquery.reddit_comments.2018_10` where subreddit = 'politics'
+union all
+select * from `fh-bigquery.reddit_comments.2018_11` where subreddit = 'politics'
+union all
+select * from `fh-bigquery.reddit_comments.2018_12` where subreddit = 'politics'
+union all
+select * from `fh-bigquery.reddit_comments.2019_01` where subreddit = 'politics'
+union all
+select * from `fh-bigquery.reddit_comments.2019_02` where subreddit = 'politics'
+union all
+select * from `fh-bigquery.reddit_comments.2019_03` where subreddit = 'politics'
+union all
+select * from `fh-bigquery.reddit_comments.2019_04` where subreddit = 'politics'
+union all
+select * from `fh-bigquery.reddit_comments.2019_05` where subreddit = 'politics'
+union all
+select * from `fh-bigquery.reddit_comments.2019_06` where subreddit = 'politics'
+union all
+select * from `fh-bigquery.reddit_comments.2019_07` where subreddit = 'politics'
+union all
+select * from `fh-bigquery.reddit_comments.2019_08` where subreddit = 'politics'
+union all
+select * from `fh-bigquery.reddit_comments.2019_09` where subreddit = 'politics';
